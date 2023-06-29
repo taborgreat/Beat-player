ec3f09ccfc35215a8a198bf868b8d60fc1c4b02d
taborgreat <taborgreat@gmail.com>
2023-06-29 15:11:37 -0700

    core logic of drums

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 
-const initialState = {
-  drumTypes: [
-    {
-      type: "kick",
-      source: new Audio(require("./kick.mp3").default),
-    },
-    {
-      type: "snare",
-      source: new Audio(require("./clap.mp3").default),
-    },
-  ]
-};
-
 function App() {
+  const initialState = {
+    drumTypes: [
+      {
+        type: "kick",
+        source: new Audio(require("./sounds/kick.mp3")),
+        key: "q",
+        img: require("./images/kick.png"),
+      },
+      {
+        type: "clap",
+        source: new Audio(require("./sounds/clap.mp3")),
+        key: "w",
+        img: require("./images/clap.png"),
+      },
+      {
+        type: "hi-hat",
+        source: new Audio(require("./sounds/hihat.mp3")),
+        key: "e",
+        img: require("./images/hihat.png"),
+      },
+      {
+        type: "openhat",
+        source: new Audio(require("./sounds/openhat.mp3")),
+        key: "r",
+        img: require("./images/openhat.png"),
+      },
+    ],
+  };
+
   const reducer = () => {};
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      console.log(event.key);
+      let currSound = state.drumTypes.filter((drum) => drum.key === event.key);
+      currSound.map((sound) => {
+        sound.source.currentTime = 0;
+        sound.source.play();
+        return null;
+      });
+    });
+  });
+
   const drumList = state.drumTypes.map((drum) => (
-    <button key={drum.type} onClick={() => drum.source.play()}>
-      {drum.type}
+    <button
+      key={drum.type}
+      onClick={() => {
+        drum.source.currentTime = 0;
+        drum.source.play();
+      }}
+    >
+      <img src={drum.img} alt={drum.type}></img>
+      <p>{drum.key}</p>
     </button>
   ));
 
   return (
     <div id="drum-machine">
-      <h1>beat player</h1>
-
       <div id="melody"></div>
 
       <div id="drums">{drumList}</div>
