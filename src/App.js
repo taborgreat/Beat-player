ec5fa3228dd4e61ea6a34ea1294812755119ef60
taborgreat <taborgreat@gmail.com>
2023-06-29 22:11:10 -0700

    finalizing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { useEffect, useReducer } from "react";
 import "./App.css";
+import { reducer } from "./reduce";
+import {actions} from "./actions"
 
-const actions = {
-  TOGGLESONG: "toggle-song",
-  NEXTSONG: "next-song",
-  PREVSONG: "prev-song",
-  TOGGLEPAUSE: "toggle-pause",
-};
+
 
 function App() {
   const initialState = {
@@ -58,59 +55,7 @@ function App() {
     playPauseImage: require("./images/play.png"),
   };
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case actions.TOGGLESONG: {
-        state.playPauseImage = require('./images/play.png')
-
-        if (
-          state.currentSong < state.maxSongs &&
-          action.move === actions.NEXTSONG
-        ) {
-          return { ...state, currentSong: state.currentSong + 1 };
-        } else if (
-          state.currentSong >= state.maxSongs &&
-          action.move === actions.NEXTSONG
-        ) {
-          return { ...state, currentSong: 0 };
-        } else if (state.currentSong > 0 && action.move === actions.PREVSONG) {
-          return { ...state, currentSong: state.currentSong - 1 };
-        } else {
-          return { ...state, currentSong: state.maxSongs };
-        }
-      }
-
-      case actions.TOGGLEPAUSE: {
-        if (!state.melodies[state.currentSong].isPaused) {
-          action.songRef.play();
-          return {
-            ...state,
-            playPauseImage: require("./images/pause.png"),
-            melodies: state.melodies.map((melody, index) =>
-              index === state.currentSong
-                ? { ...melody, isPaused: !melody.isPaused }
-                : melody
-            ),
-          };
-        } else {
-          action.songRef.pause();
-          return {
-            ...state,
-            playPauseImage: require("./images/play.png"),
-            melodies: state.melodies.map((melody, index) =>
-              index === state.currentSong
-                ? { ...melody, isPaused: !melody.isPaused }
-                : melody
-            ),
-          };
-        }
-      }
-
-      default:
-        return state;
-    }
-  };
-
+ 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
---
+++ src/actions.js
@@ -0,0 +1,6 @@
+export const actions = {
+    TOGGLESONG: "toggle-song",
+    NEXTSONG: "next-song",
+    PREVSONG: "prev-song",
+    TOGGLEPAUSE: "toggle-pause",
+  };
